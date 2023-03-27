34fdf9218447807b0208816fae84d68ad69c457c
danychaddad <dany.chaddad@gmail.com>
2023-03-27 14:20:20 +0300

    Fixed crash

--- app/src/main/java/com/shaygang/campybara/MainActivity.kt
+++ app/src/main/java/com/shaygang/campybara/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 
+// mongodb
+// username : application
+// password : fd2mZeDzVQ4rPvNY
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
