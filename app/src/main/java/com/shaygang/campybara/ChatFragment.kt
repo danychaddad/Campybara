af326e9775f3b11c71b2292cd9850284e186265e
danychaddad <dany.chaddad@gmail.com>
2023-03-27 13:07:52 +0300

    Changed colors and app icon

--- app/src/main/java/com/shaygang/campybara/ChatFragment.kt
+++ app/src/main/java/com/shaygang/campybara/ChatFragment.kt
@@ -16,6 +16,8 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ChatFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+
 class ChatFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
