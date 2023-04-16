ffa5dc708e8196eab1d4e8a1988caf5ff67750a6
danychaddad <dany.chaddad@gmail.com>
2023-04-16 12:59:05 +0300

    Creating campsites with images
    Can now upload an image for the campsite

--- app/src/main/java/com/shaygang/campybara/CreateCampsiteActivity.kt
+++ app/src/main/java/com/shaygang/campybara/CreateCampsiteActivity.kt
@@ -1,16 +1,23 @@
 package com.shaygang.campybara
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import com.shaygang.campybara.databinding.ActivityCreateCampsiteBinding
+import java.io.IOException
 import java.lang.Integer.parseInt
+import java.util.*
+
 
 class CreateCampsiteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateCampsiteBinding
+    private var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_campsite)
@@ -22,7 +29,11 @@ class CreateCampsiteActivity : AppCompatActivity() {
         }
 
         binding.confirmBtn.setOnClickListener {
-            confirmCampsiteCreation()
+            uploadImageToFirebaseStorage()
+        }
+
+        binding.selectPhoto.setOnClickListener {
+            chooseImage()
         }
     }
 
@@ -30,8 +41,40 @@ class CreateCampsiteActivity : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
-
-    private fun confirmCampsiteCreation() {
+    private fun chooseImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, 0)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0 && resultCode == RESULT_OK && data != null && data.data != null) {
+            imageUri = data.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+                binding.selectPhotoView.setImageBitmap(bitmap)
+                binding.selectPhoto.alpha = 0f
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+    private fun uploadImageToFirebaseStorage() {
+        if (imageUri != null) {
+            val filename = UUID.randomUUID().toString()
+            val ref = FirebaseStorage.getInstance().getReference("/images_campsite/$filename")
+            ref.putFile(imageUri!!)
+                .addOnSuccessListener {
+                    ref.downloadUrl.addOnSuccessListener {
+                        confirmCampsiteCreation(it.toString())
+                    }
+                }
+                .addOnFailureListener {
+                    Log.d("CreateCampsiteActivity", "Image could not be uploaded !!")
+                }
+        }
+    }
+    private fun confirmCampsiteCreation(imageUrl: String) {
         var name = binding.csNameET.text.toString()
         var description = binding.csDescET.text.toString()
         var capacity : Int
@@ -47,8 +90,9 @@ class CreateCampsiteActivity : AppCompatActivity() {
         if (capacity < 1) {
             Toast.makeText(this, "Capacity must be greater than 0!", Toast.LENGTH_SHORT).show()
         } else {
+
             val ref = FirebaseDatabase.getInstance().getReference("campsites")
-            val campsite = Campsite(name, description, capacity)
+            val campsite = Campsite(name, description, capacity, imageUrl)
             ref.push().setValue(campsite).addOnSuccessListener {
                 Toast.makeText(this,"Successfully added campsite!", Toast.LENGTH_SHORT).show()
                 exitCampsiteCreation()
@@ -57,4 +101,4 @@ class CreateCampsiteActivity : AppCompatActivity() {
     }
 }
 
-class Campsite(val name: String, val description: String, val capacity : Int)
+class Campsite(val name: String, val description: String, val capacity : Int, val imageUrl : String)
