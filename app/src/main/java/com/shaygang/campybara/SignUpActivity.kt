ef718489eb2bb04e02354d277e37225dfd2a3a2d
fwehbe <fareswhb@gmail.com>
2023-04-17 17:58:54 +0300

    Update to Sign Up Page

--- app/src/main/java/com/shaygang/campybara/SignUpActivity.kt
+++ app/src/main/java/com/shaygang/campybara/SignUpActivity.kt
@@ -1,17 +1,18 @@
 package com.shaygang.campybara
 
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.shaygang.campybara.databinding.ActivitySignUpBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -44,6 +45,25 @@ class SignUpActivity : AppCompatActivity() {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
+
+        binding.dateOfBirth.setOnClickListener {
+            val myCalendar = Calendar.getInstance()
+            val datePicker = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                myCalendar.set(Calendar.YEAR, year)
+                myCalendar.set(Calendar.MONDAY, month)
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateOfBirth(myCalendar)
+            }
+            DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+    }
+
+    private fun updateDateOfBirth(myCalendar: Calendar) {
+        val dateFormat = "dd-mm-yyyy"
+        val sdf = SimpleDateFormat(dateFormat, Locale.US)
+        binding.dateOfBirth.setText(sdf.format(myCalendar.time))
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -57,18 +77,22 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun performRegister() {
-        val username = binding.username.text.toString()
+        val firstName = binding.firstName.text.toString()
+        val lastName = binding.lastName.text.toString()
+        val phoneNb = binding.phoneNumber.text.toString()
+        val dateOfBirth = binding.dateOfBirth.text.toString()
         val email = binding.emailEt.text.toString()
         val pass = binding.passET.text.toString()
         val confirmPass = binding.confirmPassEt.text.toString()
 
-        if (selectedPhotoUri != null && username.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+        if (selectedPhotoUri != null && firstName.isNotEmpty() && lastName.isNotEmpty() && phoneNb.isNotEmpty() && dateOfBirth.isNotEmpty()
+            && email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
             if (pass == confirmPass) {
                 firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
                     if (it.isSuccessful) {
                         uploadImageToFirebaseStorage()
                     } else {
-                        Toast.makeText(this, "Sign Up Unsuccessful !!", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Email taken or invalid !!", Toast.LENGTH_SHORT).show()
                     }
                 }
             } else {
@@ -90,7 +114,7 @@ class SignUpActivity : AppCompatActivity() {
                     }
                 }
                 .addOnFailureListener {
-                    Log.d("SignUpActivity", "Image could not be uploaded !!")
+                    Toast.makeText(this, "Image could not be uploaded !!", Toast.LENGTH_SHORT).show()
                 }
         }
     }
@@ -98,19 +122,24 @@ class SignUpActivity : AppCompatActivity() {
 
     private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
         val uid = FirebaseAuth.getInstance().uid ?: ""
-        val username = binding.username.text.toString()
-        val email = binding.emailEt.text.toString()
         val ref = FirebaseDatabase.getInstance().getReference("users")
 
-        val user = User(uid, username, email, profileImageUrl)
+        val firstName = binding.firstName.text.toString()
+        val lastName = binding.lastName.text.toString()
+        val phoneNb = binding.phoneNumber.text.toString()
+        val dateOfBirth = binding.dateOfBirth.text.toString()
+        val email = binding.emailEt.text.toString()
+
+        val user = User(uid, firstName, lastName, phoneNb, dateOfBirth, email, profileImageUrl)
         ref.child(uid).setValue(user)
             .addOnSuccessListener {
                 Toast.makeText(this, "Sign Up Successful !!", Toast.LENGTH_SHORT).show()
             }
             .addOnFailureListener {
-                Log.d("SignUpActivity", "User not saved in database !!")
+                Toast.makeText(this, "User not saved in database !!", Toast.LENGTH_SHORT).show()
             }
     }
 }
 
-class User(val uid: String, val username: String, val email: String, val profileImageUrl: String)
+class User(val uid: String, val firstName: String, val lastName: String, val phoneNb: String, val dateOfBirth: String,
+           val email: String, val profileImageUrl: String)
