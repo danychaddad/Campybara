d730ea7906ce83a1af7d8b4e98eda2094b58cd40
danychaddad <dany.chaddad@gmail.com>
2023-05-03 15:24:19 +0300

    Deleted Campsites.kt Started work on refactoring Campsite.kt

--- app/src/main/java/com/shaygang/campybara/Campsite.kt
+++ app/src/main/java/com/shaygang/campybara/Campsite.kt
@@ -1,3 +1,7 @@
 package com.shaygang.campybara
 
-class Campsite(val name: String, val description: String? = "No description provided", val capacity: Int? = -1, val imageUrl: String?, val rating: Double? = 0.0, val ownerUID: String, val location: ArrayList<Double>) {}
+class Campsite(val name: String, val description: String? = "No description provided", val capacity: Int? = -1, val imageUrl: String?, val rating: Double? = 0.0, val ownerUID: String, val location: ArrayList<Double>) {
+    companion object {
+
+    }
+}
--- app/src/main/java/com/shaygang/campybara/Campsites.kt
+++
@@ -1,4 +0,0 @@
-package com.shaygang.campybara
-
-data class Campsites(var campsiteImageURL : String, var campsiteName: String) {}
-
